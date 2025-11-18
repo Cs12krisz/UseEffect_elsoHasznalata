6ac021ca2fd983e5d4b15276656c24c0a15dfa42
Csiger Imre Krisztián <csigerik@KKSZKI.LOCAL>
2025-11-18 11:30:56 +0100

    Sikeres lekérés egy végpontról

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import schoolLogo from './assets/kando.jpg'
 import './App.css'
 
+
+
+const ListaKomponens = ({elemek}) => (
+  <ol>
+    {elemek.map((elem, index) => (
+      <li key={index}>{elem.id}-{elem.name}-{elem["image_url"]}</li>
+    ))}
+  </ol>
+)
+
 export const App = () => {
-  const [count, setCount] = useState(0)
+  const [adatok, setAdatok] = useState([])
+  const [serverURL, setServerURL] = useState("https://pizza.sulla.hu/pizza")
+
+
+  useEffect(() => {
+    fetch(serverURL)
+    .then((res) => (res.ok ? res.json() : []))
+    .then((tartalom) => setAdatok(tartalom))
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://www.kkszki.hu" target="_blank"><img src={schoolLogo} alt="Iskola logó" width="200" /></a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          a számláló állása: {count}
-        </button>
-        <p>
-          Módosítsd az <code>src/App.jsx</code>-t és mentsd le, hogy tesztelhesd a HMR-t!
-        </p>
-      </div>
-      <p className="read-the-docs">
-         Kattints a React, a Kandó, vagy a Vite logókra, további információkért!
-      </p>
+     <ListaKomponens elemek={adatok}></ListaKomponens>
     </>
   )
 }
 
-export default App
+export default App;
